978d5d65cd5b94614818cf4ac8686f3b18c9cd1b
DESKTOP-KKV1KKP\Mohammad Magdy <mohammed.magdy.ali.96@gmail.com>
2019-10-10 14:36:40 +0200

    app build script for android

---
+++ buildSaffApp.ps1
@@ -0,0 +1,5 @@
+cd D:\MMS\toi-staff
+ionic cordova build android --release --prod
+cd D:\MMS\toi-staff\platforms\android\app\build\outputs\apk\release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ToiStaff.jks app-release-unsigned.apk ToiStaff -storepass 123456
+C:\Users\'Mohammad Magdy'\AppData\Local\Android\Sdk\build-tools\28.0.3\zipalign -v 4 app-release-unsigned.apk ToiStaff.apk
